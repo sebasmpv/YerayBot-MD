5ae4d2ae280d6fb37f4e4065f24a17673a451ad0
sebasmvp <160799216+sebasmvp@users.noreply.github.com>
2024-05-03 13:53:47 -0500

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,7 +1,7 @@
 import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
 const handler = async (m, {text, command, args, usedPrefix}) => {
-  if (!text) throw `*📌 𝑰𝒏𝒈𝒓𝒆𝒔𝒆 𝑼𝒏 𝑻𝒆𝒙𝒕𝒐 𝑷𝒂𝒓𝒂 𝑯𝒂𝒃𝒍𝒂𝒆 𝑪𝒐𝒏 𝑺𝒊𝒏𝒊 𝒐 𝒀𝒆𝒓𝒂𝒚*\n\n*𝑬𝒋𝒆𝒏𝒑𝒍𝒐 : ${usedPrefix + command} 𝑯𝒐𝒍𝒂 𝒀𝒆𝒓𝒂𝒚*`;
+  if (!text) throw `*📌 𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙐𝙣 𝙏𝙚𝙭𝙩𝙤 𝙋𝙖𝙧𝙖 𝙋𝙡𝙖𝙩𝙞𝙘𝙖𝙧 𝘾𝙤𝙣 𝙎𝙞𝙢𝙨𝙞𝙢𝙞 𝙊 𝙔𝙚𝙧𝙖𝙮\n\n𝑬𝒋𝒆𝒏𝒑𝒍𝒐 : ${usedPrefix + command} hola yeray*`;
   try {
     const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
     const resSimi = await api.json();
